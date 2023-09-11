77c9b59d3998fa4e9faf67b7c1ba9899fc7f8357
Трунов Владимир Владимирович <vtrunov@ispras.ru>
2023-09-11 08:22:26 +0300

    first version without placeholders and member functions done

--- Bind/bind.hpp
+++ Bind/bind.hpp
@@ -1,75 +1,62 @@
 #pragma once
 
 #include "args_list.hpp"
+#include <iostream>
 #include <memory>
 #include <type_traits>
 
 namespace MySTL {
 
-template <typename Func, typename... Args> class bind {
+template <typename Func, typename... BinderArgs> class bind {
   public:
     // TODO: Add perfect forwarding for Func also (need to add possobility for
     // containing Func&)
-    bind(Func func, Args &&...args)
-        : func_(std::make_unique<CallableImpl<Func>>(std::move(func))),
-          args_list_(std::forward<Args>(args)...)
+    bind(Func func, BinderArgs &&...args)
+        : func_(std::move(func)), args_list_(std::forward<BinderArgs>(args)...)
     {
+        call(make_indices<10>{}, std::forward<BinderArgs>(args)...);
     }
 
     template <typename... CallArgs> void operator()(CallArgs &&...args)
     {
-        return func_->call(std::forward<CallArgs>(args)...);
+        // return call(std::forward<CallArgs>(args)...);
     }
 
   private:
-    // TODO: Create just 1 struct for processing all calls,
-    // struct should be temlate with call-operator, should process
-    // members and free-fcuntions calls
-    struct CallableInterface {
-        virtual void call(Args &&...args) = 0;
-        virtual ~CallableInterface()      = default;
-    };
+    template <std::size_t... Indices, typename... Args>
+    void call(const indices<Indices...> &, Args &&...args)
+    {
+        struct empty_list {
+            empty_list(Args &&...args) {}
+        };
 
-    template <typename Callable> struct CallableImpl : CallableInterface {
-        CallableImpl(Callable func) : invoker_(std::move(func)) {}
+        using args_list_t =
+            std::conditional_t<sizeof...(Args) == 0, empty_list, args_list<Args...>>;
+        args_list_t args_lst{std::forward<Args>(args)...};
 
-        void call(Args &&...args) override
+        return invoker_.invoke(func_, std::forward<BinderArgs>(args)...);
+    }
+
+    struct MemberFunctionInvoker {
+        template <typename ObjType>
+        void invoke(const Func &func, ObjType &&obj, BinderArgs &&...args)
         {
-            return invoker_.invoke(std::forward<Args>(args)...);
+            return (obj.func)(std::forward<BinderArgs>(args)...);
         }
-
-        struct InvokerBase {
-            InvokerBase(Callable func) : func_(std::move(func)) {}
-
-            Callable func_;
-
-            ~InvokerBase() = default;
-        };
-
-        struct MemberFunctionInvoker : InvokerBase {
-            MemberFunctionInvoker(Callable func) : InvokerBase(func) {}
-
-            template <typename ObjType> void invoke(ObjType &&obj, Args &&...args)
-            {
-                return (obj.f)(std::forward<Args>(args)...);
-            }
-        };
-
-        struct FreeFunctionInvoker : InvokerBase {
-            FreeFunctionInvoker(Callable func) : InvokerBase(func) {}
-
-            void invoke(Args &&...args)
-            {
-                return this->func_(std::forward<Args>(args)...);
-            }
-        };
-
-        using invoker_t = std::conditional_t<std::is_member_function_pointer_v<Callable>,
-                                             MemberFunctionInvoker, FreeFunctionInvoker>;
-        invoker_t invoker_;
     };
 
-    std::unique_ptr<CallableInterface> func_;
-    args_list<Args...>                 args_list_;
+    struct FreeFunctionInvoker {
+        void invoke(const Func &func, BinderArgs &&...args)
+        {
+            return func(std::forward<BinderArgs>(args)...);
+        }
+    };
+
+    using invoker_t = std::conditional_t<std::is_member_function_pointer_v<Func>,
+                                         MemberFunctionInvoker, FreeFunctionInvoker>;
+
+    Func                     func_;
+    args_list<BinderArgs...> args_list_;
+    invoker_t                invoker_;
 };
 } // namespace MySTL
